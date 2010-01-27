1d9e01c6758bdca75ac7684a6f5dca41ec012f1e
timb <timb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-27 21:08:10 +0000

    support for spaces in file names
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/review@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/revision.R
+++ R/revision.R
@@ -1,5 +1,8 @@
 `revision` <-
 function(file=logRoot()){
+	hasSpace <- regexpr(' ',file) > 0
+	isQuoted <- regexpr('^["\']',file) > 0
+	if(hasSpace & !isQuoted) file <- paste("'",file,"'",sep='')
 	text <- repoInfo(file)
 	x <- xmlTreeParse(text,asText=TRUE)
 	y <- x$doc$children$info[["entry"]][["commit"]]$attributes[["revision"]]
